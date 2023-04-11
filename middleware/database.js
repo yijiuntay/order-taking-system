dd84419001f11be77d4083fe89e9d7d2fc062120
yjtay <yjtay@axrail.com>
2023-04-12 02:40:12 +0800

    Modified mongodb middleware function

--- middleware/database.js
+++ middleware/database.js
@@ -1,14 +1,14 @@
-import clientPromise from "../lib/mongodb";
-//import { ClientPromise } from "mongodb";
+//import clientPromise from "../lib/mongodb";
+import { MongoClient } from "mongodb";
 import nextConnect from "next-connect";
 
-//const client = new ClientPromise(process.env.MONGODB_URI, {
-//  useNewUrlParser: true,
-//  useUnifiedTopology: true,
-//});
+const client = new MongoClient(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 async function database(req, res, next) {
-  const client = await clientPromise;
+  //const client = await clientPromise;
   await client.connect();
   req.dbClient = client;
   req.db = client.db("fcc-mongodb-and-mongoose");
